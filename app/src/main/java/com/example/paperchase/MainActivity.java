f6c25f025fd9558c40805827669d8e76902c83d4
lordardee <viktortrpkoski@gmail.com>
2021-02-11 16:00:47 +0100

    Added splash screen

--- app/src/main/java/com/example/paperchase/MainActivity.java
+++ app/src/main/java/com/example/paperchase/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.paperchase;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+
     }
 }
