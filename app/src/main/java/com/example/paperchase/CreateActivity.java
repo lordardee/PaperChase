befc18e3e2dbf9fd463b9dbef3b5c0ead5c84801
lordardee <viktortrpkoski@gmail.com>
2021-02-14 23:19:58 +0100

    Need to save generated qr

--- app/src/main/java/com/example/paperchase/CreateActivity.java
+++ app/src/main/java/com/example/paperchase/CreateActivity.java
@@ -8,16 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import java.util.Queue;
+import java.util.LinkedList;
 
 import com.google.zxing.WriterException;
+
+import java.util.LinkedList;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
 public class CreateActivity extends AppCompatActivity {
     EditText qrvalue;
-    Button generateQR,saveQR;
+    Button generateQR,saveQR,checkCourse;
     ImageView qrCodeImage;
+    Queue<String> savedQR = new LinkedList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class CreateActivity extends AppCompatActivity {
         qrvalue = findViewById(R.id.qrInput);
         generateQR = findViewById(R.id.generateQR);
         saveQR = findViewById(R.id.saveQR);
+        checkCourse = findViewById(R.id.checkCourse);
         qrCodeImage = findViewById(R.id.qrPlaceHolder);
 
         generateQR.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +45,18 @@ public class CreateActivity extends AppCompatActivity {
                     QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, 500);
                     Bitmap qrBits = qrgEncoder.getBitmap();
                     qrCodeImage.setImageBitmap(qrBits);
+                }
+            }
+        });
+
+        saveQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = qrvalue.getText().toString();
+                if (data.isEmpty()) {
+                    qrvalue.setError("Value is required");
+                } else {
+                    savedQR.add(data);
                 }
             }
         });
